058ea37809274995ec534a3915c5cfa32bf2285b
AnuragGarimella <anurag.garimella@mitratech.com>
2020-08-23 15:20:54 +0530

    Basic react app(x86 JsEngineSwitcher)

---
+++ ReactDemo/Scripts/Tutorial.jsx
@@ -0,0 +1,9 @@
+﻿class CommentBox extends React.Component {
+    render() {
+        return (
+            <div className="commentBox">Namaste! I'm a comment Box.</div>
+            );
+    }
+}
+
+ReactDOM.render(<CommentBox />, document.getElementById('content'));

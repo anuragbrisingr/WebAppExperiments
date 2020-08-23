2f3a76fd65c194f06494788ff5dbf25da731e87f
AnuragGarimella <anurag.garimella@mitratech.com>
2020-08-23 18:23:37 +0530

    Controlled component form introduced.

--- ReactDemo/Scripts/Tutorial.jsx
+++ ReactDemo/Scripts/Tutorial.jsx
@@ -69,11 +69,28 @@ class CommentList extends React.Component {
 }
 
 class CommentForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { author: '', text: '' };
+        this.handleAuthorChange = this.handleAuthorChange.bind(this);
+        this.handleTextChange = this.handleTextChange.bind(this);
+        this.handlePost = this.handlePost.bind(this);
+    }
+    handleAuthorChange(e) {
+        this.setState({ author: e.target.value });
+    }
+    handleTextChange(e) {
+        this.setState({ text: e.target.value });
+    }
+    handlePost(e) {
+        alert(this.state.author);
+        e.preventDefault();
+    }
     render() {
         return (
             <form className="commentForm">
-                <input type="text" placeholder="Your name" />
-                <input type="text" placeholder="What's your opinion?" />
+                <input type="text" placeholder="Your name" value={this.state.author} onChange={this.handleAuthorChange} />
+                <input type="text" placeholder="What's your opinion?" value={this.state.text} onChange={this.handleTextChange} />
                 <input type="submit" value="Post" />
             </form>
             );

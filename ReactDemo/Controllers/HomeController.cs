77e9e8db3255dd0b6f2ad1a27742699d5ae90ace
AnuragGarimella <anurag.garimella@mitratech.com>
2020-08-23 16:05:23 +0530

    Addded a CommentModel class to mimic DB.

--- ReactDemo/Controllers/HomeController.cs
+++ ReactDemo/Controllers/HomeController.cs
@@ -1,9 +1,39 @@
-﻿using System.Web.Mvc;
+﻿using ReactDemo.Models;
+using System.Collections;
+using System.Collections.Generic;
+using System.Web.Mvc;
 
 namespace ReactDemo.Controllers
 {
     public class HomeController : Controller
     {
+        private static readonly IList<CommentModel> _comments;
+
+        static HomeController()
+        {
+            _comments = new List<CommentModel>
+            {
+                new CommentModel
+                {
+                    Id = 1,
+                    Author = "Rama Krishna",
+                    Text = "Namaste! React world, this is Rama Krishna."
+                },
+                new CommentModel
+                {
+                    Id = 2,
+                    Author = "Raghu Ram",
+                    Text = "Comment by Raghu."
+                },
+                new CommentModel
+                {
+                    Id = 3,
+                    Author = "Arjuna Veera",
+                    Text = "Comment."
+                }
+            };
+        }
+
         // GET: Home
         public ActionResult Index()
         {
